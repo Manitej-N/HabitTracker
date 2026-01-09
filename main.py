1afaea02e9c2fcc7e48b7830237dcc35414f168e
Manitej Narayanadasu <manitej2425@gmail.com>
2026-01-09 20:10:47 +0000

    edited the main.py --> case add_habit and lst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # A Personal project to track my Habits and maintain a Routine.
-from models import *
+from models import Habit
 #Main Menu
 def main_menu():
     """Display the main menu"""
@@ -15,17 +15,29 @@ def loop():
     while command != "quit":
         key = command.split()
         match key[0]:
-            case "add_habit":
 
+            case "add_habit":
                 print("Added Habit")
+
+                habit_name = input("Enter habit name: ")
+                habit_date = input("Enter date (YYYY-MM-DD): ")
+
+                new = Habit(habit_name, habit_date)
+                _agent.append(new)
+
+                return new
+
             case "lst":
                 print("Listed Habits")
-                print()
+                for habit in _agent:
+                    print(habit)
+
         line = input('>>> ')  # displays a command-line prompter for users to enter command script
         words = line.split(' ')  # separates the command from the script arguments
         command, args = words[0], words[1:]  # command is one of the interpreter script commands outlined in the help above
 
 if __name__ == "__main__":
+    _agent = []
     loop()
 
 
--- models.py
+++ models.py
@@ -10,26 +10,16 @@ class Habit:
     class HabitAgent:
         """<This is the documentation for HabitAgent. Complete the docstring for this class."""
 
-        def __init__(self, habit_data):  # DO NOT CHANGE
-            self._agent = self.__gen_agent(habit_data)  # data structure containing Mail objects DO NOT CHANGE
+        def __init__(self):  # DO NOT CHANGE
+            _agent = []
 
-        # Given email_data (string containing each email on a separate line),
-        # __gen_mailbox returns mailbox as a list containing received emails as Mail objects
-        @classmethod
-        def __gen_agent(cls, habit_data):  # DO NOT CHANGE
-            """ generates habits data structure
-                :ivar: String
-                :rtype: list  """
-            agent = []
-            for e in habit_data:
-                msg = e.split('\n')
-                agent.append(
-                    Habit(msg[0].split(":")[1], msg[1].split(":")[1]))
-            return agent
 
     def add_habits(self):
         """Add the habits to the list"""
-        pass
+
+
+
+
 
     def list_habits(self):
         """List all habits"""
